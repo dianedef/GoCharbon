d357923a4a6de4b300de9dc289bdf3a2e11f697f
Diane Defores <deforesd@gmail.com>
2025-02-10 13:01:50 +0100

    ok

--- src/components/pills/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './components/Button.astro';
-export { default as Card } from './components/Card.astro';
-export { default as Pill } from './components/Pill.astro';
-export { default as Config } from './config';
